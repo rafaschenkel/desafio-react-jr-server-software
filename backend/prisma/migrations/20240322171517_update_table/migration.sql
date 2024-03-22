487952bdb4951f5ae4593bd591f2c7a066530a33
Rafael Schenkel <rafaschenkel.dev@gmail.com>
2024-03-22 14:18:11 -0300

    fix: update model Products

---
+++ backend/prisma/migrations/20240322171517_update_table/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Products" (
+    "codigo" SERIAL NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "preco" DECIMAL(10,2) NOT NULL,
+    "data_cadastro" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Products_pkey" PRIMARY KEY ("codigo")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Products_descricao_key" ON "Products"("descricao");
